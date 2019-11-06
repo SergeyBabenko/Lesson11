a9aea43a440793667157b2963d08fce9ce4d0ac6
Sergey Babenko <sergey.babenko.v@outlook.com>
2019-11-06 13:18:45 +0300

    Brackets in the code. Check if the brackets in this code are correctly placed

---
+++ ru.tinkoff.src/Task11.1.kt
@@ -0,0 +1,32 @@
+fun main() {
+    val input = readLine()
+    val stack = mutableListOf<Char>()
+    val indexStack = mutableListOf<Int>()
+    val leftBraces = listOf('(', '[', '{')
+    val rightBraces = listOf(')', ']', '}')
+    var errorPosition = -1
+    if (input != null) {
+        for (i in input.indices) {
+            if (errorPosition < 0)
+                if (input[i] in leftBraces) {
+                    stack.add(input[i])
+                    indexStack.add(i + 1)
+                } else if (input[i] in rightBraces)
+                    if (stack.isEmpty())
+                        errorPosition = i
+                    else {
+                        indexStack.removeAt(indexStack.size - 1)
+                        when (input[i]) {
+                            ')' -> if (stack.last() == '(') stack.removeAt(stack.size - 1) else errorPosition = i
+                            '}' -> if (stack.last() == '{') stack.removeAt(stack.size - 1) else errorPosition = i
+                            ']' -> if (stack.last() == '[') stack.removeAt(stack.size - 1) else errorPosition = i
+                        }
+                    }
+        }
+        when {
+            errorPosition >= 0 -> println(errorPosition + 1)
+            stack.isNotEmpty() -> print(indexStack.last())
+            else -> println("Success")
+        }
+    }
+}
